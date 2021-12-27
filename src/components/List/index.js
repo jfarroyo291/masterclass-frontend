a7c3464ff40021d7f1cebc60424a67c522437b1b
jfarroyo291 <j-a-m291@hotmail.com>
2021-12-27 12:11:16 -0500

    fix(Set-correct-list): List

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -3,7 +3,7 @@ import ListItem from './ListItem';
 
 function List (props){
     return(
-        <div class="col-12">{
+        <div className="col-12">{
             props.list.map(item =>(
                 <ListItem key={item._id} item={item} />
                 )
